4ee68891ff2fa33d77c469f63fc85434a53eb13a
ezDan4ik <78163807+ezDan4ik@users.noreply.github.com>
2023-04-27 18:47:52 +0300

    Update Program.cs

--- Laba4GIT/Laba4GIT/Program.cs
+++ Laba4GIT/Laba4GIT/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,31 @@ namespace Laba4GIT
     {
         static void Main(string[] args)
         {
+            int choice;
+            do
+            {
+
+                Console.WriteLine("Сюсюкало Д. - 1");
+                Console.WriteLine("Дяченко М. - 2");
+                Console.WriteLine("Мачульський А. - 3");
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        ezdan4ik ezdan4ik = new ezdan4ik();
+                        ezdan4ik.Main();
+                        break;
+                    case 2:
+                        dito dito = new dito();
+                        dito.Main();
+                        break;
+                    case 3:
+                        holly holly = new holly();
+                        holly.Main();
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 }
